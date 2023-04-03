33dbdcd779ede6d19c2d0595844f330a5e8a8248
Samson Goodenough <goodenough.samson@gmail.com>
2023-04-02 20:31:58 -0400

    Added soft deletion for students

--- php/src/migrate.php
+++ php/src/migrate.php
@@ -27,13 +27,13 @@
       $sql = "CREATE DATABASE IF NOT EXISTS cp476;
     USE cp476;
     
-    CREATE TABLE
-      `students` (
-        `id` int NOT NULL,
-        `name` varchar(255) NOT NULL,
-        `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        PRIMARY KEY (`id`)
-      ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    CREATE TABLE `students` (
+      `id` int NOT NULL,
+      `name` varchar(255) NOT NULL,
+      `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      `deleted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
      
     CREATE TABLE
       `marks` (
