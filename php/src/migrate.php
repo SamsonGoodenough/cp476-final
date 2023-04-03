6d78646c505f31be256e82392e76d582f7177d98
Samson Goodenough <goodenough.samson@gmail.com>
2023-04-02 20:37:01 -0400

    Fix soft delete

--- php/src/migrate.php
+++ php/src/migrate.php
@@ -31,7 +31,7 @@
       `id` int NOT NULL,
       `name` varchar(255) NOT NULL,
       `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      `deleted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      `deleted_at` timestamp NULL DEFAULT NULL,
       PRIMARY KEY (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
      
