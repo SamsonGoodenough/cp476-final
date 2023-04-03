2a7c472ac114f3948017790f37bb80a07859164d
mr-rjh3 <mrrjh3@gmail.com>
2023-04-03 00:56:05 -0400

    minor changes

--- php/src/connect_db.php
+++ php/src/connect_db.php
@@ -7,8 +7,11 @@ $host = 'db';
 $user = 'root';
 $pass = getenv('MYSQL_ROOT_PASSWORD');
 
+const sanitize = '/[^\p{L}\p{N}\s]/u';
+
 // check the MySQL connection status
 $conn = new mysqli($host, $user, $pass, 'cp476');
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } else {
